4e582cff4f93e7ac97c7aba8468cf120051d934e
x13Bit <158310267+x13Bit@users.noreply.github.com>
2024-03-22 14:27:27 +0400

    new struct

--- src/signals/ai_signal_list.py
+++
@@ -1,4 +0,0 @@
-class AiSignalList(list):
-    
-    def __init_subclass__(cls) -> None:
-        return super().__init_subclass__()
--- src/signals/di_signal_list.py
+++
@@ -1,4 +0,0 @@
-class DiSignalList(list):
-    
-    def __init_subclass__(cls) -> None:
-        return super().__init_subclass__()
