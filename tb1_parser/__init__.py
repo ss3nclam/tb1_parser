cc30092bb36a075a3534edeec11697e33e31b77b
Gleb Rodionov <137335455+ss3nclam@users.noreply.github.com>
2024-04-07 18:02:02 +0400

    upd

--- tb1_parser/__init__.py
+++ tb1_parser/__init__.py
@@ -1,14 +1,3 @@
-# from . import _signal, ai_signal, di_signal, do_signal, parsed_tb1_collection, signals_collection, tb1_readed_sheets_collection
-
-# from . import _regex_lib
-# from . import _tb1_file_reader
-# from . import _tb1_sheet_reader
-# from . import _sheet_parser
-# from . import _ai_sheet_parser
-# from . import _di_sheet_parser
-# from . import _do_sheet_parser
-# from . import tb1_parser
-
 from . import signal
 from . import ai_signal
 from . import di_signal
