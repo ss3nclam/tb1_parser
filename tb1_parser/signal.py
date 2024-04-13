62f326021f6e577b86bf1c8a53701d16c7081187
Gleb Rodionov <137335455+ss3nclam@users.noreply.github.com>
2024-04-13 22:37:31 +0400

    refact

--- tb1_parser/signal.py
+++ tb1_parser/signal.py
@@ -16,8 +16,8 @@ class Signal(object):
 
 
     def __repr__(self) -> str:
-        params: str = ', '.join(f'{param_name}: {param_value}' for param_name, param_value in self.__dict__.items())
-        return f'{self.__class__.__name__}({params})'
+        params: str = '(' + ', '.join(f'{param_name}: {param_value}' for param_name, param_value in self.__dict__.items()) + ')'
+        return self.__class__.__name__ + params
     
 
     def isreserv(self) -> bool:
