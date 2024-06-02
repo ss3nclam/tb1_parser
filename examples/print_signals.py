100a68d16973247ae1a26db9b6435b08e4d539e6
Gleb Rodionov <137335455+ss3nclam@users.noreply.github.com>
2024-06-02 23:51:38 +0400

    upd example

--- examples/print_signals.py
+++ examples/print_signals.py
@@ -2,17 +2,24 @@ import os
 
 from tb1_parser import ParsedTB1Collection, TB1Parser
 
-parser = TB1Parser('temp/table.xlsx')
+parser = TB1Parser("temp/table.xlsx")
 parser.read()
+
+tb1: ParsedTB1Collection = parser.collection
+
+
+def print_signals(filtered_collection: ParsedTB1Collection):
+    for signal_type, signals in filtered_collection.items():
+        if len(signals):
+            boarder: str = f" {signal_type} ".center(
+                os.get_terminal_size().columns, "="
+            )
+            print(f"\n{boarder}\n")
+            for signal in signals:
+                print(signal)
+
 
 my_filter = lambda signal: signal.isused() and signal.isprotected()
 
-tb1: ParsedTB1Collection = parser.collection
-filtered_collection = tb1.filter(key=my_filter)
-
-for signal_type, signals in filtered_collection.items():
-    if len(signals):
-        boarder: str = f' {signal_type} '.center(os.get_terminal_size().columns, '=')
-        print(f'\n{boarder}\n')
-        for signal in signals:
-            print(signal)
+if filtered_signals := tb1.filter(key=my_filter):
+    print_signals(filtered_signals)
