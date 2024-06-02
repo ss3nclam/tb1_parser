02e6834827876a0d3c4d3e3567e9e9bbbe6c97d2
Gleb Rodionov <137335455+ss3nclam@users.noreply.github.com>
2024-06-03 03:53:46 +0400

    upd

--- examples/print_signals.py
+++ examples/print_signals.py
@@ -1,4 +1,4 @@
-import os
+from re import search
 
 from tb1_parser import ParsedTB1Collection, TB1Parser
 
@@ -7,19 +7,12 @@ parser.read()
 
 tb1: ParsedTB1Collection = parser.collection
 
+my_filters = {
+    "Резервные:": lambda x: not x.isused(),
+    "Сигналы с защитой:": lambda x: x.isprotected(),
+    "Температурные:": lambda x: search(r"[Тт]емп", x.name),
+}
 
-def print_signals(filtered_collection: ParsedTB1Collection):
-    for signal_type, signals in filtered_collection.items():
-        if len(signals):
-            boarder: str = f" {signal_type} ".center(
-                os.get_terminal_size().columns, "="
-            )
-            print(f"\n{boarder}\n")
-            for signal in signals:
-                print(signal)
-
-
-my_filter = lambda signal: signal.isused() and signal.isprotected()
-
-if filtered_signals := tb1.filter(key=my_filter):
-    print_signals(filtered_signals)
+for name, filter in my_filters.items():
+    if filtered_signals := tb1.filter(key=filter):
+        print(f"\n{name}\n" + str(filtered_signals))
