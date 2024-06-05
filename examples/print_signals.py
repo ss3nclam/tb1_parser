04384ba170212227e9a25279b100e789cae7e335
Gleb Rodionov <137335455+ss3nclam@users.noreply.github.com>
2024-06-05 21:06:03 +0400

    upd

--- examples/print_signals.py
+++ examples/print_signals.py
@@ -1,19 +1,20 @@
 import re
+from pprint import pprint
 
 from tb1_parser import ParsedTB1Collection, TB1Parser
 
 tb1 = TB1Parser("temp/table.xlsx")
 tb1.read()
 
-signals: ParsedTB1Collection = tb1.collection
+tb1_collection: ParsedTB1Collection = tb1.collection
 
 my_filters = {
     "Температурные": lambda x: re.search(r"[Тт]емп", x.name),
     "Сигналы с защитой": lambda x: x.isprotected(),
     "Резервные": lambda x: not x.isused(),
-    "Резервные": 12
 }
 
 for name, filter in my_filters.items():
-    if filtered_signals := signals.filter(key=filter):
-        print(f"\n{name} ({filtered_signals.count}):\n" + str(filtered_signals))
+    if filtered_signals := tb1_collection.filter(key=filter):
+        print(f"\n{name}:")
+        pprint(filtered_signals)
