3da7e7b3675dbd2d3d5dd0f076e82a3e534debe9
Gleb Rodionov <137335455+ss3nclam@users.noreply.github.com>
2024-08-18 21:41:06 +0400

    Refacted signals classes init method

--- tb1_parser/ai_signal.py
+++ tb1_parser/ai_signal.py
@@ -8,15 +8,15 @@ class AiSignal(Signal):
         Дата-класс для аналоговых входных сигналов, потомок 'Signal'.
         '''
         super().__init__()
-        self.unit: None | str
-        self.LL: None | float
-        self.LA: None | float
-        self.LW: None | float
-        self.HW: None | float
-        self.HA: None | float
-        self.HL: None | float
-        self.LE: None | float
-        self.HE: None | float
+        self.unit: None | str = None
+        self.LL: None | float = None
+        self.LA: None | float = None
+        self.LW: None | float = None
+        self.HW: None | float = None
+        self.HA: None | float = None
+        self.HL: None | float = None
+        self.LE: None | float = None
+        self.HE: None | float = None
 
     def isprotected(self) -> bool:
         return (self.LA or self.HA) is not None
--- tb1_parser/do_signal.py
+++ tb1_parser/do_signal.py
@@ -10,4 +10,4 @@ class DoSignal(Signal):
         Дата-класс для дискретных выходных сигналов, потомок 'Signal'.
         '''
         super().__init__()
-        self.logic_value: None | Literal[1, 0]
+        self.logic_value: None | Literal[1, 0] = None
